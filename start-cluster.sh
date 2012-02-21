4d8de896b9f34e5a7d95fb195cba4c61f7de8335
Mathieu Gauthron <mathieu@matlux.net>
2012-02-21 18:14:17 +0000

    rename cache-server properties file

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./target/classes/scripts/launchers/sh/launcher.sh server
+./target/classes/scripts/launchers/sh/launcher.sh cache-server
 
 ./target/classes/scripts/launchers/sh/launcher.sh proxy
 ./target/classes/scripts/launchers/sh/launcher.sh mbean-connector
