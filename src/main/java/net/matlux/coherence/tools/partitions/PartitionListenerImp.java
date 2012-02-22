967c19ecd525642901b3cb34491f0cca70fb30c0
Mathieu Gauthron <mathieu@matlux.net>
2012-02-22 17:59:16 +0000

    Improved the quality of logging done by the PartitionListener

--- src/main/java/net/matlux/coherence/tools/partitions/PartitionListenerImp.java
+++ src/main/java/net/matlux/coherence/tools/partitions/PartitionListenerImp.java
@@ -5,25 +5,57 @@ import net.matlux.logging.log4j.ChainsawTest;
 
 import org.apache.log4j.Logger;
 
+import com.tangosol.net.Member;
 import com.tangosol.net.partition.PartitionEvent;
 import com.tangosol.net.partition.PartitionListener;
 
 
 public class PartitionListenerImp implements PartitionListener {
 	
-	private static final Logger log = Logger.getLogger(ChainsawTest.class);
+	private static final Logger log = Logger.getLogger(PartitionListenerImp.class);
 
 	@Override
 	public void onPartitionEvent(PartitionEvent partitionEvent) {
 		final int eventType = partitionEvent.getId();
+
+		//log.debug(eventType + ", " + partitionEvent.toString());
 		
+		String evName=null;
+		String name= System.getProperty("tangosol.coherence.member");
+
 		switch (eventType) {
-		case PartitionEvent.PARTITION_LOST: // the one we really need
-		case PartitionEvent.PARTITION_TRANSMIT_ROLLBACK: // just in case
-		case PartitionEvent.PARTITION_TRANSMIT_COMMIT: // just in case
+		case PartitionEvent.PARTITION_LOST:				evName = "LOST"; break;
+		case PartitionEvent.PARTITION_TRANSMIT_BEGIN:	evName = "TRANSMIT_BEGIN"; break;
+		case PartitionEvent.PARTITION_TRANSMIT_COMMIT:	evName = "TRANSMIT_COMMIT";  break;
+		case PartitionEvent.PARTITION_TRANSMIT_ROLLBACK:evName = "TRANSMIT_ROLLBACK";  break;
+		case PartitionEvent.PARTITION_RECEIVE_BEGIN:	evName = "RECEIVE_BEGIN";  break;
+		case PartitionEvent.PARTITION_RECEIVE_COMMIT:	evName = "RECEIVE_COMMIT";  break;
+		case PartitionEvent.PARTITION_ASSIGNED:			evName = "ASSIGNED";  break;
 			
-			log.debug("Partition Event=" + eventType);
+			
+		default:
+			evName="unkown";
+			
 		}
+		String fromMember = getFromMember(partitionEvent);
+		String toMember = getToMember(partitionEvent);
+		
+		log.debug(name + " says "+partitionEvent.getPartitionSet() + " " + evName +
+				" from " + fromMember +
+				" to " + toMember);
+	}
+
+	private String getFromMember(PartitionEvent partitionEvent) {
+		Member from = partitionEvent.getFromMember();
+		String fromSet = null;
+		if (from!=null ) fromSet = from.getMemberName();
+		return fromSet;
+	}
+	private String getToMember(PartitionEvent partitionEvent) {
+		Member from = partitionEvent.getToMember();
+		String fromSet = null;
+		if (from!=null ) fromSet = from.getMemberName();
+		return fromSet;
 	}
 	
 }
