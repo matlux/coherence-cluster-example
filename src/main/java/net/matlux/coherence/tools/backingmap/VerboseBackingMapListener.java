a1b6c9db95e5b318074dff884ba649810a6b01f9
Mathieu Gauthron <mathieu@matlux.net>
2012-02-24 17:15:09 +0000

    Added member and partition number to the log of the backing map listener

--- src/main/java/net/matlux/coherence/tools/backingmap/VerboseBackingMapListener.java
+++ src/main/java/net/matlux/coherence/tools/backingmap/VerboseBackingMapListener.java
@@ -1,7 +1,5 @@
 package net.matlux.coherence.tools.backingmap;
 
-import net.matlux.coherence.tools.partitions.PartitionListenerImp;
-
 import org.apache.log4j.Logger;
 
 import com.tangosol.net.BackingMapManagerContext;
@@ -11,15 +9,27 @@ public class VerboseBackingMapListener extends AbstractMultiplexingBackingMapLis
 	 
 	private static final Logger log = Logger.getLogger(VerboseBackingMapListener.class);
 
+	private String member = System.getProperty("tangosol.coherence.member");
+
+	private BackingMapManagerContext context;
 	
     public VerboseBackingMapListener(BackingMapManagerContext context) {
         super(context);
+    	this.context = context;
     }
      
     @Override
     protected void onBackingMapEvent(MapEvent mapEvent, Cause cause) {
-         
-    	log.debug(String.format("Thread: %s Cause: %s Event: %s\n", Thread.currentThread().getName(), cause, mapEvent));
+    	String str = mapEvent.toString();
+    	String str2send=null;
+    	if(str.length() > 1000) str2send = str.substring(0, 1000);
+    	str2send=str;
+    	
+    	Object objKey = mapEvent.getKey();
+    	Object binaryKey = context.getKeyToInternalConverter().convert(objKey);
+    	int partitionNb = context.getKeyPartition(binaryKey);
+    	
+    	log.debug(String.format(member + " [part=" + partitionNb +"]: Thread: %s Cause: %s Event: %s\n", Thread.currentThread().getName(), cause, str2send));
        //System.out.printf("Thread: %s Cause: %s Event: %s\n", Thread.currentThread().getName(), cause, mapEvent);
          
     }
