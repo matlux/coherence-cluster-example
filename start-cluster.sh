65df08cf10f68d4493b0ffd28aca7c7c382fd087
Mathieu Gauthron <mathieu@matlux.net>
2012-02-26 22:21:39 +0000

    added a second TCP-extend proxy

--- start-cluster.sh
+++ start-cluster.sh
@@ -4,4 +4,5 @@
 ./target/classes/scripts/launchers/sh/launcher.sh cache-server2
 
 ./target/classes/scripts/launchers/sh/launcher.sh proxy
+./target/classes/scripts/launchers/sh/launcher.sh proxy2
 ./target/classes/scripts/launchers/sh/launcher.sh mbean-connector
