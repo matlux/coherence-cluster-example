374bddf5d34d41f0511c4ffea9a085ad0396afd4
Mathieu Gauthron <mathieu@matlux.net>
2012-02-26 01:51:02 +0000

    added a force stop all of the cluster nodes. It does a ps -ef research of the processes that have launcher and then kills them

--- stop-cluster.sh
+++ stop-cluster.sh
@@ -2,4 +2,9 @@
 
 #kill -9 `cat /tmp/server.pid`
 
+if [ "$1" = "force" ]; then
+    for pid in `ps -ef | grep launcher | grep -v grep | awk 'BEGIN{OFS=FS=" "} {print $2}'` ;do kill $pid ; done
+    exit
+fi
+
 ./target/classes/scripts/launchers/sh/launcher.sh stopall
